adabe04e3df18e1e755c8e40385e0532745535fb
Sean <52542229+sungbinlee@users.noreply.github.com>
2023-05-24 13:28:36 +0900

    Create 20.js

---
+++ JavaScript/20.js
@@ -0,0 +1,17 @@
+const babaYaga = {
+  name: "John Wick",
+  age: 53,
+  from: "벨라루스",
+    askingHim: function(){
+        console.log("Yeah, I'm thinking I'm back!");
+    }
+};
+
+const babaYaga = {
+    name: "John Wick",
+    age: 53,
+    from: "벨라루스",
+    askingHim(){
+        console.log("Yeah, I'm thinking I'm back!");
+    }
+};
